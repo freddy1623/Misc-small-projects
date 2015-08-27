4ed821f1ae7028253494946a08e85729e29f3d3a
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2015-08-27 22:51:38 +0200

    Uploaded FizzBuzz Implementation with Ternary
     had to use the printf statements  inside because char* and  int can't be displayed with same %-label thing

---
+++ FizzBuzzTernary.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for(int i = 0; i < 100; i++)
+        i % 3 == 0 && i % 5 == 0 ? printf("FizzBuzz\n") : i % 3 == 0 ? printf("Fizz\n") : i % 5 == 0 ? printf("Buzz\n") : printf("%i\n", i);
+}
