19ee58e177e65ef57b53d519a82a0c161f74a413
freddy1623 <freref98@gmail.com>
2015-10-29 09:10:52 +0100

    Update Make Users.ps1
    Removed wait hack +added `" that i forgot in request

--- Commissions/Fredrik/Make Users.ps1
+++ Commissions/Fredrik/Make Users.ps1
@@ -18,10 +18,7 @@ forEach ($User in $Users)
     #Hack to fix weird spaces between characters
     $username = $username -replace " ", ""
     #Write the command to a batfile to run in a normal cmd window
-    "dsadd user `"cn=$username, ou=Illuminati, dc=datavg2, dc=local -fn $firstname -ln $lastname -pwd Admin123" | out-file run.bat -Encoding "UTF8" -Append
-    #Hack to wait .2 seconds between each command, because Active Directory can't keep up
-    #This ip adress is reserved for documentation, so it will never reach anything. > nul is to supress the output
-    "ping 192.0.2.2 -n 1 -w 2 > nul" | out-file run.bat -Encoding "UTF8" -Append
+    "dsadd user `"cn=$username, ou=Illuminati, dc=datavg2, dc=local`" -fn $firstname -ln $lastname -pwd Admin123" | out-file run.bat -Encoding "UTF8" -Append
 }
 "echo Script has  finished running, thanks based Daniel in 1ELA for making our lives easier" | out-file run.bat -Encoding "UTF8" -Append
 "PAUSE" | out-file run.bat -Encoding "UTF8" -append
